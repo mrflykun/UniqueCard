9d985b318b820a3758a2d30ad1b60208762a052d
Fly <mrflysix97@gmail.com>
2023-12-11 20:21:58 -0300

    card

---
+++ static/css/click.js
@@ -0,0 +1,14 @@
+function sound(){
+    var snd = new Audio('../card/song/shieldWood.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+
+
+function bigImg(x) {
+    var snd = new Audio('../card/song/move02.mp3')//wav is also supported
+    snd.play()//plays the sound
+  }
+  
+  function normalImg(x) {
+
+  }
